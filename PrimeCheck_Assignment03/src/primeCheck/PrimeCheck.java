c5a7510797eef6f30c742b41fe7ec5d9e4153e89
KYLE <KYLE@KYLE-PC.frontierlocal.net>
2018-02-01 00:42:56 -0500

    toString is updated, and a few more comments added

--- PrimeCheck_Assignment03/src/primeCheck/PrimeCheck.java
+++ PrimeCheck_Assignment03/src/primeCheck/PrimeCheck.java
@@ -14,24 +14,23 @@ import java.math.BigInteger;
  *
  */
 public class PrimeCheck {
-	private String num;
-	private String prime;
+	private String num; // String called num that will be used to create a BigInteger in the isPrime method
 	/**
-	 * 
+	 * Getter for the num string
 	 * @return
 	 */
 	public String getNum() {
 		return num;
 	}
 	/**
-	 * 
+	 * Setter for the num string
 	 * @param num
 	 */
 	public void setNum(String num) {
 		this.num = num;
 	}
 	/**
-	 * Constructor for PrimeCheck Class
+	 * Constructor for PrimeCheck Class, entered as a string.
 	 * @param num
 	 */
 	public PrimeCheck(String num) {
@@ -40,7 +39,7 @@ public class PrimeCheck {
 	/**
 	 * Checks if a number is prime in BigInteger form
 	 * @param prime
-	 * @return false if it is a composite number. True if it is prime.
+	 * @return False if it is a composite number. True if it is prime.
 	 */
 	public boolean isPrime() {
 		BigInteger zero = new BigInteger("0"); // Instantiates a BigInt named zero wit the value of zero
@@ -54,16 +53,16 @@ public class PrimeCheck {
 		}															
 		for (BigInteger c = new BigInteger("3"); c.multiply(c).compareTo(num1) < 1; c = c.add(two)) { // Establishes a counter for BigInt. Next will find whether c squared is still less than num1.  
 			if (zero.equals(num1.mod(c))) {															// We add two to the counter because we only need to check the odd factors, since we already checked for all the even factors.
-			return false; 					// Checks whether the number is divisible by the counter value with a remainder of zero, returns false if it is.
+
+				return false; 					// Checks whether the number is divisible by the counter value with a remainder of zero, returns false if it is.
 			}
 		}
 		return true; // If it passes all the tests, this will return true, meaning it is a prime factor. 
 	}
-	
+	/**
+	 * toString method that will print the value of the number.
+	 */
 	public String toString() {
-		return num + " is a ";
+		return num + " is a prime number: ";
 	}
-	
-	
-
 }
