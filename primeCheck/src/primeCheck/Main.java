b4992e11753330c1b255dd6df89d5efb0e0f554c
SirPounceAlot <34079410+SirPounceAlot@users.noreply.github.com>
2018-02-01 00:49:22 -0500

    Add files via upload

---
+++ primeCheck/src/primeCheck/Main.java
@@ -0,0 +1,367 @@
+package primeCheck;
+
+
+
+import java.math.BigInteger;
+
+
+
+public class Main {
+
+
+
+	public static void main(String[] args) {
+
+		primeCheck alpha = new primeCheck("307");
+
+		System.out.println(alpha.toString() + alpha.isPrime());
+
+		if (alpha.isPrime() == true) {
+
+			System.out.println("Test alpha passed");
+
+		} else {
+
+			System.out.println("Test alpha failed");
+			
+
+		}
+		
+		primeCheck Bravo = new primeCheck("191");
+
+		System.out.println(Bravo.toString() + Bravo.isPrime());
+
+		if (Bravo.isPrime() == true) {
+
+			System.out.println("Test Bravo passed");
+
+		} else {
+
+			System.out.println("Test Bravo failed");
+
+	}
+		
+		primeCheck Charlie = new primeCheck("1000200");
+
+		System.out.println(Charlie.toString() + Charlie.isPrime());
+
+		if (Charlie.isPrime() == true) {
+
+			System.out.println("Test Charlie passed");
+
+		} else {
+
+			System.out.println("Test Charlie failed");
+			
+		}
+		
+		primeCheck Delta = new primeCheck("15485863");
+
+		System.out.println(Delta.toString() + Delta.isPrime());
+
+		if (Delta.isPrime() == true) {
+
+			System.out.println("Test Delta passed");
+
+		} else {
+
+			System.out.println("Test Delta failed");
+			
+		}
+		
+		primeCheck Echo = new primeCheck("67867967");
+
+		System.out.println(Echo.toString() + Echo.isPrime());
+
+		if (Echo.isPrime() == true) {
+
+			System.out.println("Test Echo passed");
+
+		} else {
+
+			System.out.println("Test Echo failed");
+
+		}
+		
+		primeCheck Foxtrot = new primeCheck("32452843");
+
+		System.out.println(Foxtrot.toString() + Foxtrot.isPrime());
+
+		if (Foxtrot.isPrime() == true) {
+
+			System.out.println("Test Foxtrot passed");
+
+		} else {
+
+			System.out.println("Test Foxtrot failed");
+
+		}
+		
+		primeCheck Gulf = new primeCheck("198491317");
+
+		System.out.println(Gulf.toString() + Gulf.isPrime());
+
+		if (Gulf.isPrime() == true) {
+
+			System.out.println("Test Gulf passed");
+
+		} else {
+
+			System.out.println("Test Gulf failed");
+
+		}
+		
+		primeCheck Hotel = new primeCheck("674506081");
+
+		System.out.println(Hotel.toString() + Hotel.isPrime());
+
+		if (Hotel.isPrime() == true) {
+
+			System.out.println("Test Hotel passed");
+
+		} else {
+
+			System.out.println("Test Hotel failed");
+
+		}
+		
+		primeCheck India = new primeCheck("858599503");
+
+		System.out.println(India.toString() + India.isPrime());
+
+		if (India.isPrime() == true) {
+
+			System.out.println("Test India passed");
+
+		} else {
+
+			System.out.println("Test India failed");
+
+		}
+		
+		primeCheck Juliet = new primeCheck("982451653");
+
+		System.out.println(Juliet.toString() + Juliet.isPrime());
+
+		if (Juliet.isPrime() == true) {
+
+			System.out.println("Test Juliet passed");
+
+		} else {
+
+			System.out.println("Test Juliet failed");
+
+		}
+		
+		primeCheck Kilo = new primeCheck("452930459");
+
+		System.out.println(Kilo.toString() + Kilo.isPrime());
+
+		if (Kilo.isPrime() == true) {
+
+			System.out.println("Test Kilo passed");
+
+		} else {
+
+			System.out.println("Test Kilo failed");
+
+		}
+		
+		primeCheck Mike = new primeCheck("879190852");
+
+		System.out.println(Mike.toString() + Mike.isPrime());
+
+		if (Mike.isPrime() == true) {
+
+			System.out.println("Test Mike passed");
+
+		} else {
+
+			System.out.println("Test Mike failed");
+
+		}
+		
+		primeCheck November = new primeCheck("732632797");
+
+		System.out.println(November.toString() + November.isPrime());
+
+		if (November.isPrime() == true) {
+
+			System.out.println("Test November passed");
+
+		} else {
+
+			System.out.println("Test November failed");
+
+		}
+		
+		primeCheck Oscar = new primeCheck("817504542");
+
+		System.out.println(Oscar.toString() + Oscar.isPrime());
+
+		if (Oscar.isPrime() == true) {
+
+			System.out.println("Test Oscar passed");
+
+		} else {
+
+			System.out.println("Test Oscar failed");
+
+		}
+		
+		primeCheck Papa = new primeCheck("179426323");
+
+		System.out.println(Papa.toString() + Papa.isPrime());
+
+		if (Papa.isPrime() == true) {
+
+			System.out.println("Test Papa passed");
+
+		} else {
+
+			System.out.println("Test Papa failed");
+
+		}
+		
+		primeCheck Quebec = new primeCheck("32416189381");
+
+		System.out.println(Quebec.toString() + Quebec.isPrime());
+
+		if (Quebec.isPrime() == true) {
+
+			System.out.println("Test Quebec passed");
+
+		} else {
+
+			System.out.println("Test Quebec failed");
+
+		}
+		
+		primeCheck Romeo = new primeCheck("32416188517");
+
+		System.out.println(Romeo.toString() + Romeo.isPrime());
+
+		if (Romeo.isPrime() == true) {
+
+			System.out.println("Test Romeo passed");
+
+		} else {
+
+			System.out.println("Test Romeo failed");
+
+		}
+		
+		primeCheck Siera = new primeCheck("32416189019");
+
+		System.out.println(Siera.toString() + Siera.isPrime());
+
+		if (Siera.isPrime() == true) {
+
+			System.out.println("Test Siera passed");
+
+		} else {
+
+			System.out.println("Test Siera failed");
+
+		}
+		
+		primeCheck Tango = new primeCheck("32416187899");
+
+		System.out.println(Tango.toString() + Tango.isPrime());
+
+		if (Tango.isPrime() == true) {
+
+			System.out.println("Test Tango passed");
+
+		} else {
+
+			System.out.println("Test Tango failed");
+
+		}
+		
+		primeCheck Uniform = new primeCheck("32416189493");
+
+		System.out.println(Uniform.toString() + Uniform.isPrime());
+
+		if (Uniform.isPrime() == true) {
+
+			System.out.println("Test Uniform passed");
+
+		} else {
+
+			System.out.println("Test Uniform failed");
+
+		}
+		
+		primeCheck Victor = new primeCheck("32416187567");
+
+		System.out.println(Victor.toString() + Victor.isPrime());
+
+		if (Victor.isPrime() == true) {
+
+			System.out.println("Test Victor passed");
+
+		} else {
+
+			System.out.println("Test Victor failed");
+
+		}
+		
+		primeCheck Whiskey = new primeCheck("32416189493");
+
+		System.out.println(Whiskey.toString() + Whiskey.isPrime());
+
+		if (Whiskey.isPrime() == true) {
+
+			System.out.println("Test Whiskey passed");
+
+		} else {
+
+			System.out.println("Test Whiskey failed");
+
+		}
+		
+		primeCheck Xray = new primeCheck("324161897665");
+
+		System.out.println(Xray.toString() + Xray.isPrime());
+
+		if (Xray.isPrime() == true) {
+
+			System.out.println("Test Xray passed");
+
+		} else {
+
+			System.out.println("Test Xray failed");
+
+		}
+		
+		primeCheck Yankee = new primeCheck("3241618782");
+
+		System.out.println(Yankee.toString() + Yankee.isPrime());
+
+		if (Yankee.isPrime() == true) {
+
+			System.out.println("Test Yankee passed");
+
+		} else {
+
+			System.out.println("Test Yankee failed");
+
+		}
+		
+		primeCheck Zulu = new primeCheck("32416188223");
+
+		System.out.println(Zulu.toString() + Zulu.isPrime());
+
+		if (Zulu.isPrime() == true) {
+
+			System.out.println("Test Zulu passed");
+
+		} else {
+
+			System.out.println("Test Zulu failed");
+
+		}
+		
+	}
+}
